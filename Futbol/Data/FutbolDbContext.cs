c5a52f6568c4cfda60d4eb4d4613a864fc9af782
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-30 14:58:19 +0500

    Add project files.

---
+++ Futbol/Data/FutbolDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Futbol.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Futbol.Data
+{
+    public class FutbolDbContext:DbContext
+    {
+        public FutbolDbContext(DbContextOptions<FutbolDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Futbolist> Futbolists { get; set; }
+    }
+}
---
+++ Futbol/Entites/Futbolist.cs
@@ -0,0 +1,10 @@
+﻿namespace Futbol.Entites
+{
+    public class Futbolist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Club { get; set; }
+    }
+}
