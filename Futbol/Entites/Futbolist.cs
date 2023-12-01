891f70c97536e2bf0920d16605802100a564c78f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-01 17:52:35 +0500

    Ducker-compose qoshildi

--- Futbol/Entites/Futbolist.cs
+++ Futbol/Entites/Futbolist.cs
@@ -3,8 +3,8 @@
     public class Futbolist
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Number { get; set; }
-        public string Club { get; set; }
+        public string? Name { get; set; }
+        public string? Number { get; set; }
+        public string? Club { get; set; }
     }
 }
